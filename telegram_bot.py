bfc243b36ea8267cd5ab7652cd945a289b6e0eb1
ExAdverso <alexey.slinko@gmail.com>
2025-10-26 01:05:18 +0500

    новые доки

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,7 +5,7 @@ import seaborn as sns
 import io
 import pandas as pd
 
-my_token = '8081274267:AAE9JFdgdD24iuNIou5r257Ok0gPIYNs6Zw'
+my_token = '..'
 bot = telebot.TeleBot(token=my_token)
 chat_id = '286733275'
 
